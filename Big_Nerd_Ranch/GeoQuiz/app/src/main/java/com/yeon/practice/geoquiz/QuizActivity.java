20c2a92632601656dac389dc22a98e2ab7e7ae1a
Lahavas <uv187sn@naver.com>
2017-09-15 16:12:23 +0900

    GeoQuiz Ch03 save data during device configuration changed

--- Big_Nerd_Ranch/GeoQuiz/app/src/main/java/com/yeon/practice/geoquiz/QuizActivity.java
+++ Big_Nerd_Ranch/GeoQuiz/app/src/main/java/com/yeon/practice/geoquiz/QuizActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private TextView mQuestionTextView;
 
@@ -35,6 +36,10 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
@@ -81,6 +86,13 @@ public class QuizActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
