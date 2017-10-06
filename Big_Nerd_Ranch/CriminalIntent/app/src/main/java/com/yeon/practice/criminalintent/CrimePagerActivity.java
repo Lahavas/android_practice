80ff4b7c3e169496c117f6e1d2795d4adf49c808
Lahavas <uv187sn@naver.com>
2017-10-06 21:59:21 +0900

    Ch11 integrate CrimePagerActivity

--- Big_Nerd_Ranch/CriminalIntent/app/src/main/java/com/yeon/practice/criminalintent/CrimePagerActivity.java
+++ Big_Nerd_Ranch/CriminalIntent/app/src/main/java/com/yeon/practice/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.yeon.practice.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,17 +11,30 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CRIME_ID =
+            "com.yeon.practice.criminalintent.crime_id";
 
     private List<Crime> mCrimes;
 
     private ViewPager mViewPager;
 
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
 
@@ -39,5 +54,12 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
