e96758cd7c37fd01b6dcc75ec16e16bae89c4265
Lahavas <uv187sn@naver.com>
2017-10-13 00:18:04 +0900

    Ch17 create file authority and object

--- Big_Nerd_Ranch/CriminalIntent/app/src/main/java/com/yeon/practice/criminalintent/CrimeLab.java
+++ Big_Nerd_Ranch/CriminalIntent/app/src/main/java/com/yeon/practice/criminalintent/CrimeLab.java
@@ -10,6 +10,7 @@ import com.yeon.practice.criminalintent.database.CrimeCursorWrapper;
 import com.yeon.practice.criminalintent.database.CrimeDbSchema;
 import com.yeon.practice.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -85,6 +86,11 @@ public class CrimeLab {
         } finally {
             cursor.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 
     private static ContentValues getContentValues(Crime crime) {
